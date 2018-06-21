b52188e502f303be3c5a6383d413e07f682e32cb
Francisco Lima Figueiredo <fran.fig@gmail.com>
2018-06-20 21:31:15 -0300

    Chico: Ampliando Escopo

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
-  res.render('user', { user: req.user });
+    res.render('user', { user: req.user });
 });
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/auth/login')
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/auth/login')
 }
 
 module.exports = router;
